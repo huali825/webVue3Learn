6e239caa3bbadbc2fe1350fafee5e1e296b52c40
huali0809 <1132457928@qq.com>
2024-06-28 17:05:44 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <!--结构-->
+    <div>
+        <h1>不是吧哥们 你好啊111111!!!! </h1>
+        <Person/>
+        <Person/>
+        <Person/>
+    </div>
+</template>
+
+<script lang=ts>
+    //交互
+     import Person from './components/Person.vue'
+    export default{
+        name: 'App',
+        components:{Person}
+    }
+</script>
+
+<style scoped>
+    /* 样式 */
+    .app{
+        background-color: red;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 40px;
+    }
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="person">
+    <h2>姓名: {{ a }}</h2>
+    <h2>年龄: {{ b }}</h2>
+<!--    <button @click="changeName">修改名字</button>-->
+<!--    <button @click="changeAge">修改年龄</button>-->
+<!--    <button @click="showTel">显示电话</button>-->
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  // 数据和方法
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Person',
+  setup(){
+    let name = '张三'
+    let age = 14
+    let tel = '13343434646'
+
+    return {a:name,b:age,c:tel}
+  },
+}
+</script>
+
+<style >
+  .person {
+    background-color: green;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+  button{
+    width: 100px;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+    margin: 5px;
+  }
+
+</style>
