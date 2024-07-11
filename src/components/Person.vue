6f543c97b59f4c23b4af12639795b58c67219d19
huali0809 <1132457928@qq.com>
2024-07-11 18:48:20 +0800

    修改计算属性

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,13 +2,17 @@
 <template>
   <div class="person">
     <button @click = "addName">输出全名</button> <br>
+
     <button @click = "fullNameLisi">输出李四</button> <br>
   姓: <input type="text" v-model="NameLong.firstName"> <br>
   名: <input type="text" v-model="NameLong.lastName"> <br>
-  全名: <span> {{nameTemp}}</span> <br>
+  全名变量按钮方式: <span> {{nameTemp}}</span> <br>
 
 <!--    计算属性-->
-  全名: <span> {{fullName}}</span> <br>
+  全名计算属性1: <span> {{fullName}}</span> <br>
+    <br>
+  <button @click = "changeFullName2">修改计算属性</button> <br>
+  全名计算属性2: <span> {{fullName2}}</span> <br>
 
   </div>
 </template>
@@ -38,6 +42,23 @@ import {computed, reactive, ref} from 'vue'
     console.log(1)
     return NameLong.firstName.slice(0,1).toUpperCase() + NameLong.firstName.slice(1) + '_' + NameLong.lastName
   })
+
+let fullName2 = computed({
+  get(){
+    return NameLong.firstName.slice(0,1).toUpperCase() + NameLong.firstName.slice(1) + '_' + NameLong.lastName
+  },
+  set(val){
+    const [str1,str2] = val.split('_')
+    NameLong.firstName = str1
+    NameLong.lastName = str2
+    //console.log(x)
+    console.log('set', val)
+  }
+})
+
+  function changeFullName2(){
+    fullName2.value = 'li_si'
+  }
 
   function addName () {
     NameLong.longName = NameLong.firstName  + '_' + NameLong.lastName
